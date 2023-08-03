a2fec651204079298a9d753f14f9c91622efe133
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-08-03 08:44:01 +0430

    Add show like functionality

---
+++ src/module/addLikes.js
@@ -0,0 +1,11 @@
+const appId = "hEgDPv2bsJBfvDJZuzrn";
+const apiUrl = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/`;
+
+const fetchLikes = async () => {
+  const response = await fetch(`${apiUrl}likes/`);
+  const data = await response.text();
+  const parsedata = JSON.parse(data);
+  return parsedata;
+};
+
+export { fetchLikes, apiUrl, appId };
--- src/module/addScore.js
+++ src/module/addScore.js
@@ -1,16 +1,6 @@
 const apiUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=italian';
 const apiwithID = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
 
-const addScore = async (scoreList) => {
-  const response = await fetch(apiUrl, {
-    method: 'POST',
-    headers: { 'Content-type': 'application/json' },
-    body: JSON.stringify(scoreList),
-  });
-  const data = await response.json();
-  return data.result;
-};
-
 const fetchData = async () => {
   const response = await fetch(apiUrl);
   const data = await response.json();
@@ -23,4 +13,4 @@ const fetchDataId = async (dataId) => {
   return data.meals;
 };
 
-export { addScore, fetchData, fetchDataId };
+export { fetchData, fetchDataId };
